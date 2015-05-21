7bd99d822086fb9c4865ed2622d3613309afbdfd
Luca Tumedei <luca@theaveragedev.com>
2015-05-21 14:57:53 +0200

    added a second functional test

--- tests/functional/PostPipingTest.php
+++ tests/functional/PostPipingTest.php
@@ -3,19 +3,7 @@
 
 class PostPipingTest extends \WP_UnitTestCase {
 
-	public function setUp() {
-		// before
-		parent::setUp();
-
-		// your set up methods here
-	}
-
-	public function tearDown() {
-		// your tear down methods here
-
-		// then
-		parent::tearDown();
-	}
+	protected $backupGlobals = false;
 
 	/**
 	 * @test
@@ -31,6 +19,30 @@ class PostPipingTest extends \WP_UnitTestCase {
 			'field_args'  => [
 				'name' => __( 'The post date', 'cmb2' ),
 				'id'   => cmb2_pipe( $field_id, '<', 'post_title' ),
+				'type' => 'text',
+			]
+		];
+		$field = new CMB2_Field( $args );
+
+		$value = $field->get_data( $field_id );
+
+		$this->assertEquals( 'From posts table', $value );
+	}
+
+	/**
+	 * @test
+	 * it should override the meta value when field read and writes to post field
+	 */
+	public function it_should_override_the_meta_value_when_field_read_and_writes_to_post_field() {
+		$id       = $this->factory->post->create( [ 'post_title' => 'From posts table' ] );
+		$field_id = 'the_post_title';
+		update_post_meta( $id, $field_id, 'From meta table' );
+		$args  = [
+			'object_id'   => $id,
+			'object_type' => 'post',
+			'field_args'  => [
+				'name' => __( 'The post date', 'cmb2' ),
+				'id'   => cmb2_pipe( $field_id, '<>', 'post_title' ),
 				'type' => 'text',
 			]
 		];
