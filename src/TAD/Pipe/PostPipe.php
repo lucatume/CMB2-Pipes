3e23e1a7d040ba88f86c49f9f966fdc369c02a2d
Luca Tumedei <luca@theaveragedev.com>
2015-05-27 18:24:53 +0200

    added filtering possibilities

--- src/TAD/Pipe/PostPipe.php
+++ src/TAD/Pipe/PostPipe.php
@@ -8,31 +8,37 @@ class TAD_Pipe_PostPipe extends TAD_Pipe_AbstractPipe implements TAD_Pipe_PipeIn
 	}
 
 	public function save( $override, array $args, $field_args, CMB2_Field $field ) {
-		if ( in_array( $this->direction, array( '>', '<>' ) ) ) {
-			remove_filter( "cmb2_override_{$this->field_id}_meta_save", array( $this, 'save' ) );
+		if ( $this->direction_is_read() ) {
+			return $override;
+		}
+		remove_filter( "cmb2_override_{$this->field_id}_meta_save", array( $this, 'save' ) );
 
-			$value = TAD_Pipe_PostFields::format_and_sanitize( $this->target, $args['value'] );
-			if ( $value !== TAD_Pipe_PostFields::INVALID ) {
-				wp_update_post( array( 'ID' => $args['id'], $this->target => $value ) );
-			}
+		$value = TAD_Pipe_PostFields::format_and_sanitize( $this->target, $args['value'] );
+
+		$value = $this->maybe_apply_write_filter( $value );
+
+		if ( $value !== TAD_Pipe_PostFields::INVALID ) {
+			wp_update_post( array( 'ID' => $args['id'], $this->target => $value ) );
 		}
 
 		// do override if sync, let run if write
-		return $this->direction == '>' ? $override : true;
+		return $this->direction_is_write() ? $override : true;
 	}
 
 	public function value( $override, $object_id, array $args, CMB2_Field $field ) {
-		if ( ! in_array( $this->direction, array( '<', '<>' ) ) ) {
+		if ( $this->direction_is_write() ) {
 			return $override;
 		}
 
 		$post = get_post( $args['id'], null, 'display' );
 
-		return $post->{$this->target};
+		$value = $post->{$this->target};
+
+		return $this->maybe_apply_read_filter( $value );
 	}
 
 	public function remove( $override, array $args, array $field_args, CMB2_Field $field ) {
-		if ( ! in_array( $this->direction, array( '>', '<>' ) ) ) {
+		if ( $this->direction_is_read() ) {
 			return $override;
 		}
 
