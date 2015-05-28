a4c388b8a4a34eab7d4ae2890f1dc87db41a2cf7
Luca Tumedei <luca@theaveragedev.com>
2015-05-28 21:51:59 +0200

    first user to post tests'

--- tests/functional/P2PPipeTest.php
+++ tests/functional/P2PPipeTest.php
@@ -6,7 +6,7 @@ class TAD_Pipe_P2PPipeTest extends \WP_UnitTestCase {
 	protected $backupGlobals = false;
 
 	public static function setUpBeforeClass() {
-		self::load_p2p();
+		load_p2p();
 
 		p2p_register_connection_type( [
 			'name' => 'post_to_post',
@@ -15,25 +15,6 @@ class TAD_Pipe_P2PPipeTest extends \WP_UnitTestCase {
 		] );
 	}
 
-	private static function load_p2p() {
-		require_once PLUGIN_FOLDER . '/posts-to-posts/posts-to-posts.php';
-
-		$p2p_core = PLUGIN_FOLDER . '/posts-to-posts/core';
-
-		require_once $p2p_core . '/util.php';
-		require_once $p2p_core . '/api.php';
-		require_once $p2p_core . '/autoload.php';
-
-		P2P_Autoload::register( 'P2P_', $p2p_core );
-
-		P2P_Storage::install();
-		P2P_Storage::init();
-
-		P2P_Query_Post::init();
-		P2P_Query_User::init();
-
-		P2P_URL_Query::init();
-	}
 
 	public function setUp() {
 		parent::setUp();
--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -1,2 +1,21 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+function load_p2p() {
+	require_once PLUGIN_FOLDER . '/posts-to-posts/posts-to-posts.php';
+
+	$p2p_core = PLUGIN_FOLDER . '/posts-to-posts/core';
+
+	require_once $p2p_core . '/util.php';
+	require_once $p2p_core . '/api.php';
+	require_once $p2p_core . '/autoload.php';
+
+	P2P_Autoload::register( 'P2P_', $p2p_core );
+
+	P2P_Storage::install();
+	P2P_Storage::init();
+
+	P2P_Query_Post::init();
+	P2P_Query_User::init();
+
+	P2P_URL_Query::init();
+}
